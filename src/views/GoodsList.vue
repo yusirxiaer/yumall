946ab0c08b86978133f2fc38f0944314c63aca1f
DESKTOP-0QOQ2EU\YUSIRXIAER <yusirxiaer@163.com>
2018-06-22 15:50:13 +0800

    项目创建完成首页组建已有规划

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+      <nav-header></nav-header>
+      <nav-bread></nav-bread>
+      <div class="accessory-result-page accessory-page">
+        <div class="container">
+          <div class="filter-nav">
+            <span class="sortby">Sort by:</span>
+            <a href="javascript:void(0)" class="default cur">Default</a>
+            <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+            <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+          </div>
+          <div class="accessory-result">
+            <!-- filter -->
+            <div class="filter stopPop" id="filter">
+              <dl class="filter-price">
+                <dt>Price:</dt>
+                <dd><a href="javascript:void(0)">All</a></dd>
+                <dd>
+                  <a href="javascript:void(0)">0 - 100</a>
+                </dd>
+                <dd>
+                  <a href="javascript:void(0)">100 - 500</a>
+                </dd>
+                <dd>
+                  <a href="javascript:void(0)">500 - 1000</a>
+                </dd>
+                <dd>
+                  <a href="javascript:void(0)">1000 - 2000</a>
+                </dd>
+              </dl>
+            </div>
+
+            <!-- search result accessories list -->
+            <div class="accessory-list-wrap">
+              <div class="accessory-list col-4">
+                <ul>
+                  <li v-for="(item,index) in goodsList">
+                    <div class="pic">
+                      <a href="#"><img v-bind:src="'static/'+item.productImg" alt=""></a>
+                    </div>
+                    <div class="main">
+                      <div class="name">{{item.productName}}</div>
+                      <div class="price">{{item.productPrice}}</div>
+                      <div class="btn-area">
+                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav-footer></nav-footer>
+    </div>
+</template>
+<script>
+     import './../assets/css/base.css'
+     import './../assets/css/product.css'
+     import NavHeader from '@/components/NavHeader'
+     import NavBread from '@/components/NavBread'
+     import NavFooter from '@/components/NavFooter'
+     import axios from 'axios'
+    export default{
+        data(){
+            return {
+              goodsList:[]
+            }
+        },
+        components:{
+          NavHeader,
+          NavFooter,
+          NavBread
+        },
+        mounted:function () {
+          this.getGoodsList();
+        },
+        methods:{
+          getGoodsList(){
+            axios.get('/api/goods').then((result)=>{
+              var res = result.data;
+              console.log(res);
+              this.goodsList =res.result;
+            })
+          }
+        }
+    }
+</script>
